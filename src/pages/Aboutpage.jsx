03f44a692cdfa2d6f068d40bf25a955099180d5c
a-sambhab <saisambhav21chaini@gmail.com>
2021-08-31 00:52:42 +0530

    Added Navbar

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AboutPage = () => {
     return(
         <>
-            hello
+            About Page
         </>
     );
 };
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 import './landing.css';
 const Landing = () => {
     return(
         <>
+            <Navbar/>
             <div className="landing">
                 <div className="left"></div>
-               
                 <div className="right"></div>
             </div>
         </>
