e58ddf903c6f85e349d46bf8319a89e7d755d03e
kumarshantanu01 <shantanuk280@gmail.com>
2021-09-01 02:53:57 +0530

    designers section

--- src/pages/Aboutus/LatestProjects.jsx
+++ src/pages/Aboutus/LatestProjects.jsx
@@ -3,7 +3,37 @@ import React from 'react';
 const LatestProjects = () => {
     return(
         <>
-        <h1>hello</h1>
+        <div className="latestprojects">
+            <div className="topsection-latestprojects">
+                <div className="topsection-box">
+                    <div className="leftbox-latest-projects">
+                    <img className="latestproject1" src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1630432957/alcove/pexels-houzlook-com-3797991_azbok0.jpg" alt="" />
+                        
+                    </div>
+                    <div className="rightbox-latest-projects">
+                        <img className="latestproject1" src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1630432954/alcove/about_4_yaitoy.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="bottomsection-latestprojects">
+                <div className="bottomsection-box">
+                    <div className="leftbox-latest-projects">
+                    <div className="textbox-latest-projects">
+                            <div className="heading-latest-projects">
+                            <h1>LATEST</h1> 
+                            <h1>PROJECTS</h1></div>
+                            <div className="subheading-latest-projects">
+                            <p>A company, abbreviated as co., is a legal entity representing an association of people, whether natural, legal or a mixture of both, with a specific objective. Company members share a common purpose and unite to achieve specific, declared goals.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rightbox-latest-projects">
+                    <img className="latestproject1" src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1630432960/alcove/r-architecture-TRCJ-87Yoh0-unsplash_faxyel.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
